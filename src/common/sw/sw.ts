483808ba30fcda37c0b8cd00d7a0a81b93eb6125
alwxkxk <alwxkxk@gmail.com>
2019-05-14 18:04:29 +0800

    service worker update html file every time.

--- src/common/sw/sw.ts
+++ src/common/sw/sw.ts
@@ -1,6 +1,6 @@
 // TODO: add type definition for service worker.
 // console.log("sw start.");
-const CACHE_NAME = "v20190514";
+const CACHE_NAME = "v20190515";
 
 self.addEventListener("activate", function (event) {
   console.log("sw activate.");
@@ -9,46 +9,55 @@ self.addEventListener("activate", function (event) {
 self.addEventListener("fetch", function (event) {
   // @ts-ignore
   console.log("sw request:", event.request.url);
+
   // @ts-ignore
   event.respondWith(
     // @ts-ignore
     caches.match(event.request)
       .then(function (response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
         // IMPORTANT:Clone the request. A request is a stream and
         // can only be consumed once. Since we are consuming this
         // once by cache and once by the browser for fetch, we need
         // to clone the response.
         // @ts-ignore
         const fetchRequest = event.request.clone();
+        const cache = ()=>{
+          return fetch(fetchRequest).then(
+            function (res) {
+              // Check if we received a valid response
+              if (!res || res.status !== 200 || res.type !== "basic") {
+                return res;
+              }
 
-        return fetch(fetchRequest).then(
-          function (res) {
-            // Check if we received a valid response
-            if (!res || res.status !== 200 || res.type !== "basic") {
+              // IMPORTANT:Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+
+              const responseToCache = res.clone();
+
+              caches.open(CACHE_NAME)
+                .then(function (cache) {
+                  // @ts-ignore
+                  cache.put(event.request, responseToCache);
+                });
+
               return res;
-            }
+            },
+          );
+        }
+        // Cache hit - return response
+        if (response) {
+          // html file should be update every time.
+          // @ts-ignore
+          const url = event.request.url
+          if(url.includes(".html") || url[url.length-1] === "/"){
+            cache();
+          }
+          return response;
+        }
 
-            // IMPORTANT:Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-
-            const responseToCache = res.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                // @ts-ignore
-                cache.put(event.request, responseToCache);
-              });
-
-            return res;
-          },
-        );
+        return cache();
       }),
     );
 });
