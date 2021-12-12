e2d65c47bafef761f88de561d61a66a171c3cb9f
suxiqiang <alwxkxk@gmail.com>
2021-12-12 10:02:45 +0800

    fix: webpack reload page before compile.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const config = require('./webpack.config.js')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 const devConfig = {
   ...config,
   cache: {
@@ -16,16 +17,14 @@ const devConfig = {
     static: './dist',
     hot:true,
     open:true,
-    watchFiles: ['src/**/*'],
-  },
-  // BUG: devServer dont support aggregateTimeout
-  // https://stackoverflow.com/questions/48872929/webpack-dev-server-watch-options-aggregatetimout-does-not-work
-  // https://github.com/webpack/webpack-dev-server/issues/1782
-  watch:true,
-  watchOptions:{
-    poll:1000,
-    aggregateTimeout:2000,
-    ignored:/node_modules/,
+    watchFiles: {
+      options:{
+        awaitWriteFinish:{
+          stabilityThreshold:2000
+        }
+      },
+      paths:['src/**/*']
+    }
   },
   mode:"development",//production or development
 }
